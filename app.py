b7bb29d09798cb0d38c9c3f3c6d5f4112c5ddac5
darencorp <nazar.mandebura@gmail.com>
2019-08-07 21:22:17 +0200

    fix pylint

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
+"""
+This is main file. Here are defined things such as application server,
+CORS policy and global routing between different versions of API.
+"""
+
 from flask import Flask
 from flask_cors import CORS
 
 from src.v1.routes import api_v1
 
-"""
-This is main file. Here are defined things such as application server,
-CORS policy and global routing between different versions of API.
+APP = Flask(__name__)
+CORS = CORS(APP, resources={'*': {'origins': '*'}})
 
-"""
-
-app = Flask(__name__)
-cors = CORS(app, resources={'*': {'origins': '*'}})
-
-app.register_blueprint(api_v1, url_prefix='/v1')
+APP.register_blueprint(api_v1, url_prefix='/v1')
 
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    APP.run(debug=False, host='0.0.0.0', port=5000)
